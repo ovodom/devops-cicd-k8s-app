11416806a3a623ba00948bb27394dd5e0186b4ae
Obi-Egbedi Ogheneovo Frederick <ovodom21@gmail.com>
2026-03-18 23:11:43 +0100

    Add CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is version 2 of the app!"
+    return "🚀 CI/CD SUCCESS - Version 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
